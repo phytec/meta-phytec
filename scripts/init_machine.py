a04e308bc673a2ca223de8dcb13003e193ae2806
Stefan Müller-Klieser <s.mueller-klieser@phytec.de>
2015-04-16 14:16:42 +0200

    scripts/init_machine: adapt behavior to switch_machine
    We changed switch_machine. In case of an init, the machine would be
    written two times to the localconf now. Also if the machine is set to
    UNASSIGNED the user will be asked for input selection.
    
    Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

--- scripts/init_machine.py
+++ scripts/init_machine.py
@@ -9,15 +9,15 @@ from switch_machine import *
 
 def main():
     bsp = BSP_Switcher()
-    if not bsp.selected_machine:
+    if not bsp.selected_machine or "UNASSIGNED" in bsp.selected_machine:
         # we could not parse a machine from manifest xml
         # we assume we set up an unified bsp and need to
         # ask the user for a machine to be set up
-        bsp.switch_machine()
-    if not bsp.write_machine_to_localconf():
-        # An error has happened. Report it back to calling program.
-        sys.exit(1)
-
+        if not bsp.switch_machine():
+            sys.exit(1)
+    else:
+        if not bsp.write_machine_to_localconf():
+            sys.exit(1)
 
 if __name__ == "__main__":
     main()
