phytec/meta-phytec

commit e23ebe4d4f984522727d6be56a3fefd47867916d

wic-helper: Remove zImage prefix for DTBs
Commit
c5b70d7 kernel-devicetree.bbclass: Fix and simplify instalation of DTB files

changed the name for the deployment of DTBs. Remove zImage prefix so WIC
is able to find our DTBs again.

Signed-off-by: Wadim Egorov <w.egorov@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

classes/wic-helper.bbclass
@@ -6,8 +6,8 @@ def parse_dtbs(d):
     dtbcount=1
     for DTB in kdt.split():
         if dtbcount == 1:
-            dtbs += "zImage-"+DTB+";oftree"
-        dtbs += " zImage-"+DTB
+            dtbs += DTB+";oftree"
+        dtbs += " "+DTB
         dtbcount += 1
     return dtbs
 
@@ -48,5 +48,4 @@ do_image_emmc[depends] += " \
     e2fsprogs-native:do_populate_sysroot \
     virtual/kernel:do_deploy \
     virtual/bootloader:do_deploy \
-    virtual/prebootloader:do_deploy \
 "

commit ecbb07ff42d1f0b28921f5685add547a1f332108

wic-helper: fix prebootloader dependency
Commit e23ebe4d4f accidentally removed the prebootloader dependency. Add
it again.

Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

classes/wic-helper.bbclass
@@ -49,4 +49,5 @@ do_image_emmc[depends] += " \
     e2fsprogs-native:do_populate_sysroot \
     virtual/kernel:do_deploy \
     virtual/bootloader:do_deploy \
+    virtual/prebootloader:do_deploy \
 "