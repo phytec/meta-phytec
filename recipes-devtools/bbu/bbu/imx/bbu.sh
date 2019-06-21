9a68aa5c47a23a76f3a3cc47f98626ab9eb5b54e
Jan Remmet <j.remmet@phytec.de>
2019-06-21 14:15:13 +0200

    bbu: check release from barebox images
    Checks if a barebox with the same release tag is already in flash and
    avoids to flash it again.
    The check is skipped if barebox in flash has no meta data build in.
    Adds a dependency to barebox-targettools
    
    Signed-off-by: Jan Remmet <j.remmet@phytec.de>
    Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

--- recipes-devtools/bbu/bbu/imx/bbu.sh
+++ recipes-devtools/bbu/bbu/imx/bbu.sh
@@ -2,9 +2,32 @@
 # Copyright 2018 PHYTEC Messtechnik GmbH
 
 usage() {
-	echo "$0 <BOOTLOADER>"
+	echo "$0 [-f] <BOOTLOADER>"
 	echo "BOOTLOADER	- barebox image format"
+	echo "-f		force flashing same or unkown version"
 }
+
+get_releasename() {
+	release=`bareboximd $1 2>/dev/null | grep release | awk '{print $2}'`
+	if [ -z "$release" ]; then
+		echo "get no release name from $1"
+		exit 1
+	fi
+	echo $release
+}
+
+while getopts "f" o; do
+	case "${o}" in
+		f)
+			force=true;
+			;;
+		*)
+			usage
+			exit 1
+			;;
+	esac
+done
+shift $((OPTIND-1))
 
 if [ "$#" -ne 1 ]; then
 	echo "Illegal number of parameters"
@@ -24,6 +47,29 @@ mdname=`mtdinfo /dev/mtd0 | grep Name | awk '{print $2}'`
 if [ "$mdname" != "barebox" -o "$mdtype" != "nand" ]; then
 	echo "/dev/mtd0 is not a barenbox partion on nand"
 	exit 1
+fi
+
+imagev=`get_releasename $1`
+if [ $? -ne 0 ]; then
+	echo "barebox image has no meta data skip flashing"
+	if [ -z "$force" ]; then 
+		exit 1
+	else
+		echo "but forcing"
+	fi
+fi
+
+installedv=`get_releasename /dev/mtd0ro`
+# test if installed barebox have build in meta data
+if [ $? -eq 0 ]; then
+	if [ "$installedv" == "$imagev" ]; then
+		echo "barebox release $installedv is already installed"
+		if [ -z "$force" ]; then
+			exit 0
+		else
+			echo "but forcing"
+		fi
+	fi
 fi
 
 # Update barebox
