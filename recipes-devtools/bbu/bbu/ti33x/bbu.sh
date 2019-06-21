9b7df3128b75091506887c64c65a0ca2bde76254
Jan Remmet <j.remmet@phytec.de>
2019-06-21 14:15:13 +0200

    bbu: check release from barebox images
    Checks if a barebox with the same release tag is already in flash and
    avoids to flash it again.
    The check is skipped if barebox in flash has no meta data build in.
    Adds a dependency to barebox-targettools
    
    Signed-off-by: Jan Remmet <j.remmet@phytec.de>
    Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

--- recipes-devtools/bbu/bbu/ti33x/bbu.sh
+++ recipes-devtools/bbu/bbu/ti33x/bbu.sh
@@ -2,9 +2,10 @@
 # Copyright 2018 PHYTEC Messtechnik GmbH
 
 usage() {
-	echo "$0 <MLO> <BOOTLOADER>"
+	echo "$0 [-f] <MLO> <BOOTLOADER>"
 	echo "MLO		- AM335x CH image format"
 	echo "BOOTLOADER	- barebox image format"
+	echo "-f		force flashing same or unkown version"
 }
 
 update_mtd_part() {
@@ -27,6 +28,28 @@ update_mtd_part() {
 		exit 1
 	fi
 }
+
+get_releasename() {
+	release=`bareboximd $1 2>/dev/null | grep release | awk '{print $2}'`
+	if [ -z "$release" ]; then
+		echo "get no release name from $1"
+		exit 1
+	fi
+	echo $release
+}
+
+while getopts "f" o; do
+	case "${o}" in
+		f)
+			force=true;
+			;;
+		*)
+			usage
+			exit 1
+			;;
+	esac
+done
+shift $((OPTIND-1))
 
 if [ "$#" -ne 2 ]; then
 	echo "Illegal number of parameters"
@@ -56,6 +79,28 @@ for slot in $xload_nand_slots
 do
 	nand=`mtdinfo /dev/$slot | grep Type | awk '{print $2}'`
 	if [ "$nand" = "nand" ]; then
+		imagev=`get_releasename $1`
+		if [ $? -ne 0 ]; then
+			echo "mlo image has no meta data skip flashing"
+			if [ -z "$force" ]; then 
+				continue
+			else
+				echo "but forcing"
+			fi
+		fi
+		installedv=`get_releasename /dev/$slot`
+		# test if installed barebox have build in meta data
+		if [ $? -eq 0 ]; then
+			if [ "$installedv" == "$imagev" ]; then
+				echo "mlo release $installedv is already installed"
+				if [ -z "$force" ]; then
+					continue
+				else
+					echo "but forcing"
+				fi
+			fi
+
+		fi
 		update_mtd_part $slot $1
 	fi
 done
@@ -68,6 +113,27 @@ for part in $barebox_part
 do
 	nand=`mtdinfo /dev/$part | grep Type | awk '{print $2}'`
 	if [ "$nand" = "nand" ]; then
+		imagev=`get_releasename $2`
+		if [ $? -ne 0 ]; then
+			echo "barebox image has no meta data skip flashing"
+			if [ -z "$force" ]; then 
+				continue
+			else
+				echo "but forcing"
+			fi
+		fi
+		installedv=`get_releasename /dev/$part`
+		# test if installed barebox have build in meta data
+		if [ $? -eq 0 ]; then
+			if [ "$installedv" == "$imagev" ]; then
+				echo "barebox release $installedv is already installed"
+				if [ -z "$force" ]; then
+					continue
+				else
+					echo "but forcing"
+				fi
+			fi
+		fi
 		update_mtd_part $part $2
 	fi
 done
