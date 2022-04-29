8cf2a6419e0fff225814320b092776ef14bd7a01
Stefan Riedmueller <s.riedmueller@phytec.de>
2022-04-29 15:07:48 +0200

    recipes-devtools: Add phycam setup scripts
    Add setup scripts to setup phycam media pipelines easily. This includes some
    udev rules to create consistent device nodes for important sub and video
    devices, which make it easier to detect and configure.
    
    This recipe currently only supports the i.MX 8MP phyBOARD-Pollux but
    hopefully in the future it will support other platforms as well.
    
    Signed-off-by: Stefan Riedmueller <s.riedmueller@phytec.de>
    Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

---
+++ recipes-devtools/phycam/phycam-setup/mx8mp/setup-pipeline-csi1.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+display_help() {
+	echo "Usage: ./setup-pipeline-csi1.sh [-f <format>] [-s <frame size>] [-o <offset>] [-c <sensor size>] [-v]"
+}
+
+if ! [ -L /dev/cam-csi1 ] ; then
+	echo "No camera found on CSI1"
+	exit 1
+fi
+
+CAM_ENT="$(cat /sys/class/video4linux/$(readlink /dev/cam-csi1)/name)"
+
+case $(echo ${CAM_ENT} | cut -d" " -f1) in
+	ar0144 )
+		CAM_BW_FMT="Y8_1X8"
+		CAM_COL_FMT="SGRBG8_1X8"
+		SENSOR_RES="1280x800"
+		OFFSET_SENSOR="(0,4)"
+		;;
+	ar0521 )
+		CAM_BW_FMT="Y8_1X8"
+		CAM_COL_FMT="SGRBG8_1X8"
+		SENSOR_RES="2592x1944"
+		OFFSET_SENSOR="(0,0)"
+		;;
+	* ) echo "Unknown camera" ; exit 1
+esac
+
+OPTIONS="hf:s:o:c:v"
+COLOR="$(v4l2-ctl -d /dev/cam-csi1 --get-subdev-fmt 0 | grep "Mediabus Code" | sed 's/.*BUS_FMT_\([A-Z]*\).*/\1/g')"
+if [ $COLOR = "Y" ]; then
+	FMT="${CAM_BW_FMT}"
+else
+	FMT="${CAM_COL_FMT}"
+fi
+RES="$SENSOR_RES"
+OFFSET="$OFFSET_SENSOR"
+FRES="$SENSOR_RES"
+VERBOSE=""
+
+while getopts $OPTIONS option
+do
+	case $option in
+		f ) FMT=$OPTARG;;
+		s ) RES=$OPTARG;;
+		o ) OFFSET=$OPTARG;;
+		c ) FRES=$OPTARG;;
+		v ) VERBOSE="-v";;
+		h  ) display_help; exit;;
+		\? ) echo "Unknown option: -$OPTARG" >&2; exit 1;;
+		:  ) echo "Missing option argument for -$OPTARG" >&2; exit 1;;
+		*  ) echo "Unimplemented option: -$OPTARG" >&2; exit 1;;
+	esac
+done
+
+echo "Setting ${FMT}/${RES} ${OFFSET}/${FRES} for ${CAM_ENT}"
+
+media-ctl -l "'mxc-mipi-csi2.0':4->'mxc_isi.0':0[1]" ${VERBOSE}
+media-ctl -l "'mxc_isi.0':12->'mxc_isi.0.capture':0[1]" ${VERBOSE}
+
+if [ -L /dev/phycam-deserializer-csi1 ] && [ -L /dev/phycam-serializer-csi1 ] ; then
+	DESERIALIZER="$(cat /sys/class/video4linux/$(readlink /dev/phycam-deserializer-csi1)/name)"
+	SERIALIZER="$(cat /sys/class/video4linux/$(readlink /dev/phycam-serializer-csi1)/name)"
+	media-ctl -l "'${SERIALIZER}':1->'${DESERIALIZER}':0[1]" ${VERBOSE}
+fi
+
+media-ctl -V "'${CAM_ENT}':0[fmt:${FMT}/${RES} ${OFFSET}/${FRES}]" ${VERBOSE}
+media-ctl -V "'mxc-mipi-csi2.0':0 [fmt:${FMT}/${RES} field:none]" ${VERBOSE}
