phytec/meta-phytec

commit 89d17cc7fcd023eb1ec0c51bcb4329f7181f9b77

linux-mainline: imx6ul: Update recipe version to v5.15.35-phy1
Changes from v5.10.76-phy4 to v5.15.35-phy1:
  ARM: dts: imx6: phyFLEX: disable da9063 watchdog during suspend
  dt-bindings: mfd: da9063: watchdog: add suspend disable option
  watchdog: da9063: optionally disable watchdog during suspend
  media: onsemi: core: Set default active bus
  input: tsc200x-core: add wakeup IRQ source support
  ARM: dts: imx6ul: segin: peb-av-02: add support for tsc2004 touchcontroller
  FROMLIST: Input: tsc200x-core - Add axis inversion and swapping support
  ARM: dts: imx6ul: segin: peb-av-02: Mark stmpe touch as wakeup-source
  ARM: dts: imx6ul: segin: peb-av-02: move touchcontroller to peb-av-02
  ARM: dts: imx6ul: phycore-segin: Correct node name for stmpe touch-controller
  UPSTREAM: ARM: imx_v6_v7_defconfig: change snd soc tlv320aic3x to i2c variant
  PCI: imx6: revert to link handling in driver itself
  media: i2c: onsemi-core: update to follow new v4l2-subdev api
  media: i2c: vm050: update to follow new v4l2-subdev api
  media: i2c: vm012: update to follow new v4l2-subdev api
  media: mt9m111: update to follow new v4l2-subdev api
  ...

Signed-off-by: Andrej Picej <andrej.picej@norik.com>
Acked-by: Stefan Riedmueller <s.riedmueller@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-kernel/linux/linux-mainline_5.15.%.bbappend
@@ -1,0 +1,1 @@
+RDEPENDS_${KERNEL_PACKAGE_NAME}-base += "phytec-dt-overlays"

recipes-kernel/linux/linux-mainline_5.15.35-phy1.bb
@@ -12,7 +12,7 @@ PR = "${INC_PR}.0"
 
 # NOTE: PV must be in the format "x.y.z-.*". It cannot begin with a 'v'.
 # NOTE: Keep version in filename in sync with commit id!
-SRCREV = "a23e41360faa195dd1e6e16b4a9485710f9af08a"
+SRCREV = "bd7dc9b4693c0068f79180da7d2c36fbad865671"
 
 S = "${WORKDIR}/git"
 

commit 62fa8c720fb5de861245bbaa9a22451026656f46

linux-mainline: Fix override syntax for devicetree overlays
Fix the override syntax for the devicetree overlay inclusion, otherwise
overlays won't be added to the image.

Fixes: 89d17cc7fcd0 ("linux-mainline: imx6ul: Update recipe version to v5.15.35-phy1")
Signed-off-by: Stefan Riedmueller <s.riedmueller@phytec.de>
Reviewed-by: Norbert Wesp <n.wesp@phytec.de>

recipes-kernel/linux/linux-mainline_5.15.%.bbappend
@@ -1,3 +1,3 @@
 include linux-common-hardening.inc
 
-RDEPENDS_${KERNEL_PACKAGE_NAME}-base += "phytec-dt-overlays"
+RDEPENDS:${KERNEL_PACKAGE_NAME}-base += "phytec-dt-overlays"