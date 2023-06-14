e081d48328e737b11401370a7a400d16827834c4
Maik Otto <m.otto@phytec.de>
2023-06-14 13:55:10 +0200

    recipes-devtools: nxp-cst: nxp-cst: Update to version 3.3.2
    The new revision supports the backends ssl and pkcsc11 with a parameter
    on the command line. For the older versions are built different cst binaries.
    NXP has updated srktool to generate 256-bit fuse configuration supporting
    i.MX8ULP and i.MX9x devices.
    The NXP cst binaries are compiled with OpenSSL 1.1.1t.
    
    Signed-off-by: Maik Otto <m.otto@phytec.de>
    Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

--- recipes-devtools/nxp-cst/nxp-cst/cst-wrapper.sh
+++ recipes-devtools/nxp-cst/nxp-cst/cst-wrapper.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-if [ -n "${CST_KEY_SOURCE}" ] ; then
-    if [ "${CST_KEY_SOURCE}" = "token" ] ; then
-        cst-pkcs11 $@
-        exit $?
-    fi
+arg=""
+if [ ${#@} -gt 0 ]; then
+	arg="-b ssl $@"
+	if [ -n "${CST_KEY_SOURCE}" ] ; then
+		if [ "${CST_KEY_SOURCE}" = "token" ] ; then
+			arg="-b pkcs11 $@"
+		fi
+	fi
 fi
-cst-bin $@
+cst-bin $arg
