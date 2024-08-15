57ebfdbf3b43196f1adfebfec0815c55f9178cd9
Benjamin Hahn <B.Hahn@phytec.de>
2024-08-15 11:47:27 +0200

    recipes-bsp: u-boot-imx: boot.cmd: Add support for overlays and netboot
    Add support for overlays and netboot to the bootscript. The variable
    fit_extensions is set via a function in the board code and defines the
    configuration names of the overlays/extensions that need to be applied.
    
    Signed-off-by: Benjamin Hahn <B.Hahn@phytec.de>
    Reviewed-by: Yannic Moog <y.moog@phytec.de>
    Signed-off-by: Norbert Wesp <n.wesp@phytec.de>

--- recipes-bsp/u-boot/u-boot-imx/boot.cmd
+++ recipes-bsp/u-boot/u-boot-imx/boot.cmd
@@ -1,8 +1,55 @@
-setenv bootcmd "mmc dev ${mmcdev}; if mmc rescan; then if run loadimage; then run mmcboot; fi; fi;"
 setenv image "fitImage"
-setenv loadimage "fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image}"
-setenv mmcargs "setenv bootargs console=${console} root=/dev/mmcblk${mmcdev}p${mmcroot} rootwait rw"
+setenv loadimage "fatload mmc ${devnum}:${distro_bootpart} ${loadaddr} ${image}"
+setenv mmcargs "setenv bootargs console=${console} root=/dev/mmcblk${devnum}p${mmcroot} rootwait rw"
+setenv netargs "setenv bootargs console=${console} root=/dev/nfs ip='${nfsip}' nfsroot=${serverip}:'${nfsroot}',v3,tcp"
 setenv mmcautodetect "yes"
-setenv mmcboot "echo Booting from mmc ...; run mmcargs; bootm ${loadaddr};"
+setenv fitboot "\
+if test ${no_extensions} = 0; then \
+    if env exists overlays; then \
+        bootm ${loadaddr}#${fit_fdtconf}${fit_extensions}#'${overlays}'; \
+    else \
+        bootm ${loadaddr}#${fit_fdtconf}${fit_extensions}; \
+    fi; \
+else \
+    if env exists overlays; then \
+        bootm ${loadaddr}#${fit_fdtconf}#'${overlays}'; \
+    else \
+        bootm ${loadaddr}#${fit_fdtconf}; \
+    fi; \
+fi;"
 
-run bootcmd
+if test ${devtype} = mmc; then
+	mmc dev ${devnum};
+	if run loadimage; then
+		echo Booting from mmc ...;
+		if test ${no_bootenv} = 0; then
+			if run mmc_load_bootenv; then
+				env import -t ${bootenv_addr_r} ${filesize};
+			fi;
+		fi;
+		run mmcargs;
+		run fitboot;
+	fi;
+else
+	if test ${devtype} = ethernet; then
+		if test ${ip_dyn} = yes; then
+			setenv nfsip dhcp;
+			setenv get_cmd dhcp;
+		else
+			setenv nfsip ${ipaddr}:${serverip}::${netmask}::eth0:on;
+			setenv get_cmd tftp;
+		fi;
+		if ${get_cmd} ${loadaddr} ${image}; then
+			echo Booting from net ...;
+			if test ${no_bootenv} = 0; then
+				if run net_load_bootenv; then
+					env import -t ${bootenv_addr_r} ${filesize};
+				fi;
+			fi;
+			run netargs;
+			run fitboot;
+		fi;
+	else
+		echo Ampliphy only supports mmc and ethernet boot, not ${devtype} boot;
+	fi;
+fi;
