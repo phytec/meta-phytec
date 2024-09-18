9ed97b2c4955a4eb3d1ebc1818d535a6f956635d
Stefan Riedmüller <s.riedmueller@phytec.de>
2024-09-18 18:14:15 +0200

    isp-imx-phycam: run_isp: Make use of new isp-mode-select scripts
    Use the now available mode selection scripts to initialize the ISP mode
    and lens when starting the ISP server.
    
    Signed-off-by: Stefan Riedmüller <s.riedmueller@phytec.de>
    Signed-off-by: Norbert Wesp <n.wesp@phytec.de>

--- dynamic-layers/fsl-bsp-release/recipes-bsp/isp-imx/isp-imx-phycam/run_isp.sh
+++ dynamic-layers/fsl-bsp-release/recipes-bsp/isp-imx/isp-imx-phycam/run_isp.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-AR0144_LENS="AO082"
-AR0234_LENS="AO082"
-AR0521_LENS="AO062"
-
 if ! [ -e /dev/cam-csi1 ] && \
    ! [ -e /dev/cam-csi2 ] && \
    ! [ -e /dev/cam-csi1-port0 ] && \
@@ -39,40 +35,18 @@ if [ -e /dev/cam-csi1 ] || \
    [ -e /dev/cam-csi1-port1 ]; then
 
 	if [ -e /dev/cam-csi1 ]; then
-		CAM_DEV="/dev/cam-csi1"
+		isp-mode-select-csi1
 	elif [ -e /dev/cam-csi1-port0 ] && ! [ -e /dev/cam-csi1-port1 ]; then
-		CAM_DEV="/dev/cam-csi1-port0"
+		isp-mode-select-csi1 -p 0
 		setup-pipeline-csi1 -p 0
 	elif [ -e /dev/cam-csi1-port1 ] && ! [ -e /dev/cam-csi1-port0 ]; then
-		CAM_DEV="/dev/cam-csi1-port1"
+		isp-mode-select-csi1 -p 1
 		setup-pipeline-csi1 -p 1
 	else
 		echo "No valid configuration"
 		echo "Dual port phyCAM-L not supported with ISP"
 		exit 6
 	fi
-
-
-	CAM="$(cat /sys/class/video4linux/$(readlink ${CAM_DEV})/name | \
-		cut -d" " -f1)"
-	COLOR="$(v4l2-ctl -d ${CAM_DEV} --get-subdev-fmt 0 | \
-		grep "Mediabus Code" | sed 's/.*BUS_FMT_\([A-Z]*\).*/\1/g')"
-
-	if [ $COLOR = "Y" ]; then
-		COLOR="bw"
-	else
-		COLOR="col"
-	fi
-
-	case ${CAM} in
-		ar0144) LENS=${AR0144_LENS} ;;
-		ar0234) LENS=${AR0234_LENS} ;;
-		ar0521) LENS=${AR0521_LENS} ;;
-		* ) echo "Unknown camera" ; exit 6
-	esac
-
-	ln -sf /opt/imx8-isp/bin/Sensor0_Entry.cfg.${CAM}.${LENS}.${COLOR} \
-	       /opt/imx8-isp/bin/Sensor0_Entry.cfg
 fi
 
 # Create Sensor1_Entry.cfg link according to connected Sensor and Color
@@ -81,39 +55,18 @@ if [ -e /dev/cam-csi2 ] || \
    [ -e /dev/cam-csi2-port1 ]; then
 
 	if [ -e /dev/cam-csi2 ]; then
-		CAM_DEV="/dev/cam-csi2"
+		isp-mode-select-csi2
 	elif [ -e /dev/cam-csi2-port0 ] && ! [ -e /dev/cam-csi2-port1 ]; then
-		CAM_DEV="/dev/cam-csi2-port0"
+		isp-mode-select-csi2 -p 0
 		setup-pipeline-csi2 -p 0
 	elif [ -e /dev/cam-csi2-port1 ] && ! [ -e /dev/cam-csi2-port0 ]; then
-		CAM_DEV="/dev/cam-csi2-port1"
+		isp-mode-select-csi2 -p 1
 		setup-pipeline-csi2 -p 1
 	else
 		echo "No valid configuration"
 		echo "Dual port phyCAM-L not supported with ISP"
 		exit 6
 	fi
-
-	CAM="$(cat /sys/class/video4linux/$(readlink ${CAM_DEV})/name | \
-		cut -d" " -f1)"
-	COLOR="$(v4l2-ctl -d ${CAM_DEV} --get-subdev-fmt 0 | \
-		grep "Mediabus Code" | sed 's/.*BUS_FMT_\([A-Z]*\).*/\1/g')"
-
-	if [ $COLOR = "Y" ]; then
-		COLOR="bw"
-	else
-		COLOR="col"
-	fi
-
-	case ${CAM} in
-		ar0144) LENS=${AR0144_LENS} ;;
-		ar0234) LENS=${AR0234_LENS} ;;
-		ar0521) LENS=${AR0521_LENS} ;;
-		* ) echo "Unknown camera" ; exit 6
-	esac
-
-	ln -sf /opt/imx8-isp/bin/Sensor1_Entry.cfg.${CAM}.${LENS}.${COLOR} \
-	       /opt/imx8-isp/bin/Sensor1_Entry.cfg
 fi
 
 ISP_LOG_LEVEL=4 /opt/imx8-isp/bin/isp_media_server ${RUN_OPT}
