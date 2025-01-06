phytec/meta-phytec

commit c14baf359514ccbf1be01538a9e06be49d3f660f

recipes-bsp: u-boot: boot.cmd add env exists guards
We try to "run <env varibale>" without checking if it exists first.
Depending on whether a board supports boot env and/or extensions, boot
script will crash in U-Boot:

 Booting from mmc ...
 ## Error: "mmc_load_bootenv" not defined

Add env exists guards that protects script from crashing.

Signed-off-by: Yannic Moog <y.moog@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-bsp/u-boot/u-boot-imx/boot.cmd
@@ -4,7 +4,7 @@ setenv mmcargs "setenv bootargs console=${console} root=/dev/mmcblk${devnum}p${m
 setenv netargs "setenv bootargs console=${console} root=/dev/nfs ip='${nfsip}' nfsroot=${serverip}:'${nfsroot}',v3,tcp ${optargs}"
 setenv mmcautodetect "yes"
 setenv fitboot "\
-if test ${no_extensions} = 0; then \
+if env exists no_extensions && test ${no_extensions} = 0; then \
     if env exists overlays; then \
         bootm ${loadaddr}#${fit_fdtconf}${fit_extensions}#'${overlays}'; \
     else \
@@ -22,8 +22,8 @@ if test ${devtype} = mmc; then
 	mmc dev ${devnum};
 	if run loadimage; then
 		echo Booting from mmc ...;
-		if test ${no_bootenv} = 0; then
-			if run mmc_load_bootenv; then
+		if env exists no_bootenv && test ${no_bootenv} = 0; then
+			if env exists mmc_load_bootenv && run mmc_load_bootenv; then
 				env import -t ${bootenv_addr_r} ${filesize};
 			fi;
 		fi;
@@ -41,8 +41,8 @@ else
 		fi;
 		if ${get_cmd} ${loadaddr} ${image}; then
 			echo Booting from net ...;
-			if test ${no_bootenv} = 0; then
-				if run net_load_bootenv; then
+			if env exists no_bootenv && test ${no_bootenv} = 0; then
+				if env exists net_load_bootenv run net_load_bootenv; then
 					env import -t ${bootenv_addr_r} ${filesize};
 				fi;
 			fi;

commit a76346ceca7a47d920dd980bb185936986006592

recipes-bsp: u-boot: boot.cmd: Fix env exists guards
Add missing "&&" in the netboot script.

Fixes: c14baf359514 ("recipes-bsp: u-boot: boot.cmd add env exists guards")

Signed-off-by: Andrej Picej <andrej.picej@norik.com>
Reviewed-by: Yannic Moog <y.moog@phytec.de>
Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

recipes-bsp/u-boot/u-boot-imx/boot.cmd
@@ -42,7 +42,7 @@ else
 		if ${get_cmd} ${loadaddr} ${image}; then
 			echo Booting from net ...;
 			if env exists no_bootenv && test ${no_bootenv} = 0; then
-				if env exists net_load_bootenv run net_load_bootenv; then
+				if env exists net_load_bootenv && run net_load_bootenv; then
 					env import -t ${bootenv_addr_r} ${filesize};
 				fi;
 			fi;