f5d8432374fc0e74008f8c1f11980a8aef796673
Yannic Moog <y.moog@phytec.de>
2025-01-15 14:58:53 +0100

    recipes-bsp: u-boot-master: switch to standard boot
    Add boot script and infrastructure to package and provision it to deploy
    directory. Same as for u-boot-imx.
    
    Signed-off-by: Yannic Moog <y.moog@phytec.de>
    Signed-off-by: Stefan Müller-Klieser <s.mueller-klieser@phytec.de>

--- recipes-bsp/u-boot/u-boot-master.bb
+++ recipes-bsp/u-boot/u-boot-master.bb
@@ -3,17 +3,41 @@ require recipes-bsp/u-boot/u-boot.inc
 
 inherit imx-boot-container
 
-DEPENDS += "bc-native dtc-native gnutls-native"
+DEPENDS += "bc-native dtc-native gnutls-native u-boot-mkimage-native"
 
 GIT_URL = "git://source.denx.de/u-boot/u-boot.git;protocol=https"
 SRC_URI = "${GIT_URL};branch=${BRANCH}"
 LIC_FILES_CHKSUM = "file://Licenses/README;md5=2ca5f2c35c8cc335f0a19756634782f1"
 
+PR = "r0"
+S = "${WORKDIR}/git"
+
 SRCREV = "${AUTOREV}"
 BRANCH = "master"
+
+FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}:"
+SRC_URI:append = " \
+    file://boot.cmd \
+    file://boot.its \
+    "
 
 DEFAULT_PREFERENCE = "-1"
 
 PROVIDES += "u-boot"
+
+UBOOT_ENV_FIT_SRC = "boot.its"
+UBOOT_ENV_FIT_BINARY = "boot.scr.uimg"
+
+# Use FIT image boot script
+do_compile:append() {
+    if [ -n "${UBOOT_ENV_FIT_SRC}" ]
+    then
+        ${UBOOT_MKIMAGE} -C none -A ${UBOOT_ARCH} -f ${UNPACKDIR}/${UBOOT_ENV_FIT_SRC} ${WORKDIR}/${UBOOT_ENV_FIT_BINARY}
+    fi
+}
+
+do_deploy:append() {
+    install -m 644 ${WORKDIR}/${UBOOT_ENV_FIT_BINARY} ${DEPLOYDIR}/${UBOOT_ENV_FIT_BINARY}
+}
 
 COMPATIBLE_MACHINE = "^(phyboard-polis-imx8mm-5|phyboard-pollux-imx8mp-3|phygate-tauri-l-imx8mm-2)$"
---
+++ recipes-bsp/u-boot/u-boot-master/boot.cmd
@@ -0,0 +1,55 @@
+setenv image "fitImage"
+setenv loadimage "fatload mmc ${devnum}:${distro_bootpart} ${loadaddr} ${image}"
+setenv mmcargs "setenv bootargs console=${console} root=/dev/mmcblk${devnum}p${mmcroot} fsck.repair=yes rootwait rw ${optargs}"
+setenv netargs "setenv bootargs console=${console} root=/dev/nfs ip='${nfsip}' nfsroot=${serverip}:'${nfsroot}',v3,tcp ${optargs}"
+setenv mmcautodetect "yes"
+setenv fitboot "\
+if env exists no_extensions && test ${no_extensions} = 0; then \
+    if env exists overlays; then \
+        bootm ${loadaddr}#${fit_fdtconf}${fit_extensions}#'${overlays}'; \
+    else \
+        bootm ${loadaddr}#${fit_fdtconf}${fit_extensions}; \
+    fi; \
+else \
+    if env exists overlays; then \
+        bootm ${loadaddr}#${fit_fdtconf}#'${overlays}'; \
+    else \
+        bootm ${loadaddr}#${fit_fdtconf}; \
+    fi; \
+fi;"
+
+if test ${devtype} = mmc; then
+	mmc dev ${devnum};
+	if run loadimage; then
+		echo Booting from mmc ...;
+		if env exists no_bootenv && test ${no_bootenv} = 0; then
+			if env exists mmc_load_bootenv && run mmc_load_bootenv; then
+				env import -t ${bootenv_addr_r} ${filesize};
+			fi;
+		fi;
+		run mmcargs;
+		run fitboot;
+	fi;
+else
+	if test ${devtype} = ethernet; then
+		if test ${ip_dyn} = yes; then
+			setenv nfsip dhcp;
+			setenv get_cmd dhcp;
+		else
+			setenv nfsip ${ipaddr}:${serverip}::${netmask}::eth0:on;
+			setenv get_cmd tftp;
+		fi;
+		if ${get_cmd} ${loadaddr} ${image}; then
+			echo Booting from net ...;
+			if env exists no_bootenv && test ${no_bootenv} = 0; then
+				if env exists net_load_bootenv && run net_load_bootenv; then
+					env import -t ${bootenv_addr_r} ${filesize};
+				fi;
+			fi;
+			run netargs;
+			run fitboot;
+		fi;
+	else
+		echo Ampliphy only supports mmc and ethernet boot, not ${devtype} boot;
+	fi;
+fi;
